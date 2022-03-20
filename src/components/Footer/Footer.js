ac771eb3ff8280c094c0f34d3ad98a99d3b7b7b6
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-03-20 18:16:11 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,9 +11,9 @@ function Footer(){
         <div>
         <footer className="hero-footer">
             <div className="social">
-                <a href="https://www.linkedin.com/in/saicharangouru/" target="_blank" rel="noopener noreferrer"><LinkedInIcon className="social-media-icon" style={{ fontSize: "30px" }}>></LinkedInIcon></a>
-                <a href="https://twitter.com/saicharangouru" target="_blank" rel="noopener noreferrer"><TwitterIcon className="social-media-icon" style={{ fontSize: "30px" }}>></TwitterIcon></a>
-                <a href="https://github.com/saicharan-gouru/" target="_blank" rel="noopener noreferrer"><GitHubIcon className="social-media-icon" style={{ fontSize: "30px" }}>></GitHubIcon></a>
+                <a href="https://www.linkedin.com/in/saicharangouru/" target="_blank" rel="noopener noreferrer"><LinkedInIcon className="social-media-icon" style={{ fontSize: "30px" }}></LinkedInIcon></a>
+                <a href="https://twitter.com/saicharangouru" target="_blank" rel="noopener noreferrer"><TwitterIcon className="social-media-icon" style={{ fontSize: "30px" }}></TwitterIcon></a>
+                <a href="https://github.com/saicharan-gouru/" target="_blank" rel="noopener noreferrer"><GitHubIcon className="social-media-icon" style={{ fontSize: "30px" }}></GitHubIcon></a>
             </div>
             <p>Made with 💖 by <a className="footer-credits" href="https://saicharangouru.netlify.app" target="_blank" rel="noopener noreferrer">Saicharan Gouru <LaunchIcon style={{ fontSize: "15px" }}></LaunchIcon></a></p>
             <small>ⓒ Spazeir Mart | 2022</small>
