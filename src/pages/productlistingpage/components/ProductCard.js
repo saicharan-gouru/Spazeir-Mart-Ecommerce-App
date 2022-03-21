568111942282fd17123b7754d58b74edbe4538a0
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-03-21 21:39:54 +0530

    Created product card template

---
+++ src/pages/productlistingpage/components/ProductCard.js
@@ -0,0 +1,23 @@
+import "./ProductCard.css";
+
+function ProductCard({product}){
+
+    const {title,price,imageLink} = product;
+    return(
+        <div class="product-card">
+            <div class="product-card-header">
+                <img class="hero-img " src={imageLink} />
+                <div class="content ">
+                    <h2 class="hero-heading ">{title}</h2>
+                    <h4 class="price">{price} Rupees</h4>
+                </div>
+            </div>
+            <div class="product-card-footer">
+                <button class="btn-add-to-cart">add to cart</button>
+                <button class="btn-buy-now">Buy now</button>
+            </div>
+        </div>
+    );
+}
+
+export {ProductCard};
