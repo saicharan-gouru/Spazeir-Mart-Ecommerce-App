9ed09d3848562295ed498c18b81259c2ef46f48b
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-04-01 11:47:34 +0530

    Bug fic

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -3,7 +3,7 @@ function cartReducer(state, { type, payload }) {
         case "ADD_TO_CART":
             return {...state, items_count: state.items_count + 1, itemsInCart: [...state.itemsInCart, {...payload, quantity: 1 }], total_price: state.total_price + payload.price };
         case "REMOVE_FROM_CART":
-            return {...state, items_count: state.items_count - 1, itemsInCart: state.itemsInCart.filter(product => product._id !== payload._id), total_price: state.total_price - payload.price };
+            return {...state, items_count: state.items_count - 1, itemsInCart: state.itemsInCart.filter(product => product._id !== payload._id), total_price: state.total_price - payload.price * payload.quantity };
         case "CLEAR_CART":
             return {...state, items_count: 0, total_price: 0, itemsInCart: [] }
         case "INCREASE_PRODUCT_QUANTITY":
